ff9d19603bf9ebae8a988df5d7781130ef004803
pawan-gla <pawankr.sharma@gla.ac.in>
2024-06-13 17:42:20 +0530

    first  liveML application using streamlit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import pickle as pkl
 from sklearn.datasets import fetch_openml
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -13,7 +14,8 @@ y = housing.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = LinearRegression()
 model.fit(X_train, y_train)
-
+with open("model.pkl","wb") as f:
+    pkl.dump(model,f)
 # Streamlit app
 st.title('Pawan Housing Price Prediction')
 
